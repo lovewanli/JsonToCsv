2f3cf790bd6d8cc6f24dae293952a5d467c88b28
lovewanli <lovewanli@hotmail.com>
2020-09-21 13:33:32 +0800

    Update JsonToCsv.py

--- JsonToCsv.py
+++ JsonToCsv.py
@@ -8,7 +8,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-def fileRead(filepath):
+def fileread(filepath):
     result = ""
     for line in open(filepath):
         result = result + line
@@ -17,48 +17,53 @@ def fileRead(filepath):
         key = masterline.encode('utf-8')
         typestr = type(dict1[key])
         if typestr == list:
-            listprint(dict1[key],)
+            listprint(dict1[key], )
         elif typestr == dict:
             for masterline1 in dict1[key].keys():
                 key1 = masterline1.encode('utf-8')
                 if type(dict1[key][key1]) == list:
-                    listprint(dict1[key][key1],filepath)
+                    listprint(dict1[key][key1], filepath)
 
 
-def listprint(list):
-    keys = []
-    for line in list:
-        if (len(keys) == 0):
-            lines = ''
-            for key in line.keys():
-                keys.append(key.encode('utf-8'))
-                lines = lines + '"' + key.encode('utf-8') + '",'
-            print(lines[0:len(lines) - 1])
-        else:
-            lines = ''
-            for key in keys:
-                value = 'NULL'
-                if line.has_key(key):
-                    value = str(line[key])
-                    if value.find('"'):
-                        value.replace('"', '""')
-                    valuelength = len(value)
-                if value.isdigit():
-                    if valuelength == 10:
-                        try:
-                            value = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(value)))
-                        except Exception as e:
-                            pass
-                    if valuelength == 13:
-                        try:
-                            value = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(value) / 1000))
-                        except Exception as e:
-                            pass
-                lines = lines + '"' + value + '",'
-            print(lines[0:len(lines) - 1])
-    print ''
+def listprint(lists, filepath):
+    with open(filepath + ".csv", "w") as fo:
+        fo.write('\xEF\xBB\xBF')
+        keys = []
+        for line in lists:
+            if len(keys) == 0:
+                lines = ''
+                for key in line.keys():
+                    keys.append(key.encode('utf-8'))
+                    lines = lines + '"' + key.encode('utf-8') + '",'
+                print(lines[0:len(lines) - 1])
+                fo.write(lines[0:len(lines) - 1]+'\n')
+            else:
+                lines = ''
+                for key in keys:
+                    value = 'NULL'
+                    if line.has_key(key):
+                        value = str(line[key])
+                        if value.find('"'):
+                            value.replace('"', '""')
+                        valuelength = len(value)
+                    if value.isdigit():
+                        if valuelength == 10:
+                            try:
+                                value = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(value)))
+                            except Exception as e:
+                                pass
+                        if valuelength == 13:
+                            try:
+                                value = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(value) / 1000))
+                            except Exception as e:
+                                pass
+                    lines = lines + '"' + value + '",'
+                print(lines[0:len(lines) - 1])
+                fo.write(lines[0:len(lines) - 1]+'\n')
+        print ''
+    fo.close()
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        fileRead(sys.argv[1])
+        fileread(sys.argv[1])
